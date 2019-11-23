4ecf2882da1c3ae7720dc60ef89e3ed19f935fb1
Amir Mohamad Ehsandar <amir.ehsandar@divar.ir>
2019-11-23 15:14:25 +0330

    Init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import request, jsonify, Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    print(f'request ip address: {request.remote_addr}')
+    return jsonify({'ip': request.remote_addr}), 200
+
+
+if __name__ == '__main__':
+    app.run()
